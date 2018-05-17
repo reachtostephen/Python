fcbc7f337f885d9850f8e34f52d2181f6bddf7a0
Stephen Raj <histephen2015@gmail.com>
2018-05-17 10:13:57 +0530

    Update positive.py

--- Beginner/positive.py
+++ Beginner/positive.py
@@ -1,7 +1,12 @@
-N = int(raw_input())
-if N>0 and N<10000:
-	print("Positive")
-elif N==0:
-	print("Zero")
-else:
-	print("Negative")
+try:
+	N = int(raw_input())
+
+	if N>0 and N<10000:
+		print("Positive")
+	elif N==0:
+		print("Zero")
+	else:
+		print("Negative")
+
+except:
+	print("Exception")
