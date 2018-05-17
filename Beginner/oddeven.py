4a6377cbb5c64a1ef0ad7275948fb694885098ff
Stephen Raj <histephen2015@gmail.com>
2018-05-17 10:16:21 +0530

    Update oddeven.py

--- Beginner/oddeven.py
+++ Beginner/oddeven.py
@@ -1,9 +1,10 @@
-N = int(raw_input())
+try:
+	N = int(raw_input())
 
-if N>=0:
-	if(N%2==0):
-		print("Even")
-	else:
-		print("Odd")
-else:
-	print("Not positive")
+	if N>=0:
+		if(N%2==0):
+			print("Even")
+		else:
+			print("Odd")
+except:
+	print("Exception")
